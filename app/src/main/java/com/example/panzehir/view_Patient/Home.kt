f7c3d17c63b7ff1695adfb7d1e1f4b5c0514467d
sevaldilara bicer <348291@ogr.ktu.edu.tr>
2021-12-18 16:31:53 +0300

    added emergency calling%50

--- app/src/main/java/com/example/panzehir/view_Patient/Home.kt
+++ app/src/main/java/com/example/panzehir/view_Patient/Home.kt
@@ -2,16 +2,19 @@
 
 package com.example.panzehir.view_Patient
 
+import android.app.AlertDialog
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.example.panzehir.R
 import com.example.panzehir.databinding.HomeFragmentBinding
 import com.example.panzehir.viewModelPatient.HomeViewModel
+import com.google.android.gms.tasks.Tasks.call
 
 class Home : Fragment() {
     private var _binding: HomeFragmentBinding?=null
@@ -41,5 +44,35 @@ class Home : Fragment() {
         // bottom_nav_menu_patient added 2 more items to improve the appearance
         binding.bottomNavigationView.menu.getItem(1).isEnabled = false
         binding.bottomNavigationView.menu.getItem(2).isEnabled = false
+        binding.callEmergencyLayout.setOnClickListener {
+            emergencyAlertButton()
+        }
+        binding.callFamilyLayout.setOnClickListener {
+            callMyFamilyAlertButton()
+        }
+    }
+
+    private fun callMyFamilyAlertButton() {
+       //Acil durum,Ailemi ara butonuna basılırsa
+    }
+
+    fun emergencyAlertButton(){
+        val builder = AlertDialog.Builder(this.context)
+        builder.setTitle("Arama")
+        builder.setMessage("Acil Durum Hattını Aramak İstiyor Musunuz?")
+//builder.setPositiveButton("OK", DialogInterface.OnClickListener(function = x))
+
+        builder.setPositiveButton("Ara") { dialog, which ->
+            Toast.makeText(this.context,
+                "Aranılıyor...", Toast.LENGTH_SHORT).show()
+        }
+
+        builder.setNegativeButton("Arama") { dialog, which ->
+            Toast.makeText(this.context,
+                "Vazgeçildi.", Toast.LENGTH_SHORT).show()
+        }
+        builder.show()
+
+
     }
 }
